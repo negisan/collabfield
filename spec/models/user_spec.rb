300d6e50c2a2c5e002b9a5b8e680a305c521dcac
wayne <juchuang123@gmail.com>
2021-02-16 20:00:21 +0900

    Add spec for User, Category, Post model's associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe Category, type: :model do
+RSpec.describe User, type: :model do
   context 'Association' do
     it 'has_many posts' do
       association = described_class.reflect_on_association(:post)
-      expect(association.macro).eq :has_many
+      expect(association.macro).to eq :has_many
+      expect(association.ooptions[:dependent]).to eq :destroy
     end
   end
 end
