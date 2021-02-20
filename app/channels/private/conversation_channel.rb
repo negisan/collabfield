4d5a5ac9a13499418f5cfe5e60130a389c3d0c48
wayne <juchuang123@gmail.com>
2021-02-20 09:19:06 +0900

    Make a private conversation window's new message form functional - Add an event handler inside the   javascripts/channels/private/conversation.js to trigger the submit button - Define a common behavior among conversation windows inside the   assets/javascripts/conversations/conversation.js - Define a send_message function on both, client and server, sides

--- app/channels/private/conversation_channel.rb
+++ app/channels/private/conversation_channel.rb
@@ -8,4 +8,11 @@ class Private::ConversationChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
     stop_all_streams
   end
+
+  def send_message(data)
+    message_params = data['message'].each_with_object({}) do |el, hash|
+      hash[el['name']] = el['value']
+    end
+    Private::Message.create(message_params)
+  end
 end
