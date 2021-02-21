3a870967e64a4fed32b44cb04366951c8071b5d9
wayne <juchuang123@gmail.com>
2021-02-21 10:53:12 +0900

    Get data for group conversations in ApplicationController - Update the opened_conversations_windows method - Update the OrderConversationsService

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,10 +12,15 @@ class ApplicationController < ActionController::Base
 
   def opened_conversations_windows
     if user_signed_in?
+      # opened conversations
       session[:private_conversations] ||= []
-      @privata_conversations_windows = Private::Conversation.includes(:recipient, :messages).find(session[:private_conversations])
+      session[:group_conversations] ||= []
+      @private_conversations_windows = Private::Conversation.includes(:recipient, :messages)
+                                           .find(session[:private_conversations])
+      @group_conversations_windows = Group::Conversation.find(session[:group_conversations])
     else
       @private_conversations_windows = []
+      @group_conversations_windows = []
     end
   end
 
